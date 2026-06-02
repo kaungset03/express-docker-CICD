ec6354ed5f8e58f14694ab18c23e3942ad32c468
Kaung Set <kset2299@gmail.com>
2026-06-02 17:07:03 +0630

    feat: add Docker Compose configuration for app and watchtower services

--- index.js
+++ index.js
@@ -5,16 +5,12 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
 });
 
+app.get("/goodbye", (req, res) => {
+  res.send("<h1>Goodbye</h1>");
+});
+
 const PORT = 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.get("/goodbye", (req, res) => {
-  res.send("Goodbye World!");
 });
