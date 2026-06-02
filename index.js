abd1cfe8bb0e6eab4be06e0ff635fd68ed8902f4
Kaung Set <kset2299@gmail.com>
2026-06-02 16:30:27 +0630

    refactor: standardize code formatting and improve readability

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
 
-const PORT = 8080
+const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/goodbye", (req, res) => {
+  res.send("Goodbye World!");
+});
